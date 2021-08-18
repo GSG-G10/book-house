7c5f19087797703eb1fc7273188ebc4bcce7a342
Noorin99 <noorin.sa.99@gmail.com>
2021-08-18 16:41:40 +0300

    adding pop up forms Relates #16

---
+++ public/scripts/script.js
@@ -0,0 +1,13 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.querySelector('.book').style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.querySelector('.book').style.display = "block";
+}
+
+
+
